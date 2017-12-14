abdeb833e8c902a27f781cc04e86b699669c8f73
nicolabba <nicola.abbagnato@gmail.com>
2017-12-14 15:58:26 +0100

    I colori sono difficili da capire

--- Model/colore.cpp
+++ Model/colore.cpp
@@ -1,8 +1,77 @@
 #include "colore.h"
-
-int Colore::getShade(Primari p) const
+Colore::Colore():prim1(), prim2(), prim3(){}
+Colore::Colore(int _prim1, int _prim2, int _prim3):prim1(_prim1>MIN?_prim1<MAX?_prim1:MAX:MIN),
+                                                   prim2(_prim2>MIN?_prim2<MAX?_prim2:MAX:MIN),
+                                                   prim3(_prim3>MIN?_prim3<MAX?_prim3:MAX:MIN){}
+Colore::Colore(const Colore &c)
 {
-    switch (p) {
+    Colore(c.prim1,c.prim2,c.prim3);
+}
+Colore Colore::operator +(const Colore &c)
+{
+    return Colore(prim1 + c.prim1,prim2 + c.prim2,prim3 + c.prim3);
+}
+Colore Colore::operator -(const Colore &c)
+{
+    return Colore(prim1 - c.prim1,prim2 - c.prim2,prim3 - c.prim3);
+}
+Colore Colore::operator /(const Colore &c)
+{
+    return Colore((prim1 + c.prim1) / 2,(prim2 + c.prim2) / 2,(prim3 + c.prim3) / 2);
+}
+bool Colore::operator ==(const Colore& c)
+{
+    return (prim1 == c.prim1 && prim2 == c.prim2 && prim3 == c.prim3);
+}
+void Colore::add(const Colore &c)
+{
+    *this = *this + c;
+}
+void Colore::remove(const Colore &c)
+{
+    *this = *this + c;
+}
+void Colore::media(const Colore &c)
+{
+    *this = *this + c;
+}
+void Colore::setShade(int i, int amount)
+{
+    switch (i) {
+    case 1:
+        if(amount > MIN)
+            if(amount < MAX)
+                prim1 = amount;
+            else
+                prim1 = MAX;
+        else
+            prim1 = MIN;
+        break;
+    case 2:
+        if(amount > MIN)
+            if(amount < MAX)
+                prim2 = amount;
+            else
+                prim2 = MAX;
+        else
+            prim2 = MIN;
+        break;
+    case 3:
+        if(amount > MIN)
+            if(amount < MAX)
+                prim3 = amount;
+            else
+                prim3 = MAX;
+        else
+            prim3 = MIN;
+        break;
+    default:
+        break;
+    }
+}
+int Colore::getShade(int i) const
+{
+    switch (i) {
     case 1:
         return prim1;
         break;
@@ -15,3 +84,72 @@ int Colore::getShade(Primari p) const
     }
 }
 
+bool ColoreRGB::checkType(const Colore &c) const
+{
+    return typeid(c) == typeid(const ColoreRGB&);
+}
+ColoreRGB()
+{
+    Colore();
+}
+
+ColoreRGB(int red, int green, int blue)
+{
+    Colore(red,green,blue);
+}
+
+ColoreRGB(const Colore &c)
+{
+    if(checkType(c))
+    {
+        Colore(c);
+    }
+}
+
+ColoreRGB ColoreRGB::operator +(const Colore &c) const
+{
+    if(checkType(c))
+    {
+        return static_cast<const ColoreRGB&>(Colore::operator +(c));
+    }
+}
+
+ColoreRGB ColoreRGB::operator -(const Colore &c) const
+{
+    if(checkType(c))
+    {
+        return static_cast<const ColoreRGB&>(Colore::operator -(c));
+    }
+}
+
+ColoreRGB ColoreRGB::operator /(const Colore &c) const
+{
+    if(checkType(c))
+    {
+        return static_cast<const ColoreRGB&>(Colore::operator /(c));
+    }
+}
+void ColoreRGB::add(const Colore &c)
+{
+    *this = *this + c;
+}
+void ColoreRGB::remove(const Colore &c)
+{
+    *this = *this - c;
+}
+void ColoreRGB::media(const Colore &c)
+{
+    *this = *this / c;
+}
+int ColoreRGB::getShade(Primari p) const
+{
+    return Colore::getShade((int)p);
+}
+void ColoreRGB::setShade(Primari p, int amount)
+{
+    Colore::setShade((int)p, amount);
+}
+ColoreRGB::operator ==(const Colore &c)
+{
+    return (checkType(c) && Colore::operator ==(c));
+}
--- Model/colore.h
+++ Model/colore.h
@@ -5,29 +5,44 @@
 class Colore
 {
 private:
-    int Prim1, Prim2, Prim3;
+    int prim1, prim2, prim3;
+protected:
+    Colore();
+    Colore(int _prim1,int _prim2,int _prim3);
+    Colore(const Colore& c);
+    int getShade(int i) const;
+    void setShade(int i, int amount);
+    virtual bool checkType(const Colore&c) const = 0;
 public:
-    enum Primari{};
+    static int const MIN = 0;
+    static int const MAX = 255;
     virtual void add(const Colore& c);
     virtual void remove(const Colore& c);
     virtual void media(const Colore& c);
-    virtual Colore& operator+(const Colore& c) const;
-    virtual Colore& operator-(const Colore& c) const;
+    virtual Colore operator+(const Colore& c) const;
+    virtual Colore operator-(const Colore& c) const;
+    virtual Colore operator/(const Colore& c) const;
     virtual bool operator==(const Colore& c) const;
-    int getShade(Primari p) const;
-    void setShade(Primari p, int amount);
 };
 
 class ColoreRGB : public Colore
 {
+private:
+    bool checkType(const Colore &c) const;
 public:
     enum Primari{Red, Green, Blue};
+    ColoreRGB();
+    ColoreRGB(int red,int green,int blue);
+    ColoreRGB(const Colore& c);
     void add(const Colore& c);
     void remove(const Colore& c);
     void media(const Colore& c);
-    ColoreRGB& operator+(const Colore& c) const;
-    ColoreRGB& operator-(const Colore& c) const;
+    ColoreRGB operator+(const Colore& c) const;
+    ColoreRGB operator-(const Colore& c) const;
+    ColoreRGB operator/(const Colore& c) const;
     bool operator==(const Colore& c) const;
+    int getShade(Primari p) const;
+    void setShade(Primari p, int amount);
 };
 
 #endif // COLORE_H
